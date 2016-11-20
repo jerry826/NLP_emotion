9f52c9e0e870413895c2d2f278bb112636d59bfc
Jerry Lv <jiayang826@qq.com>
2016-11-20 19:19:41 +0800

    Add model function and utilis function

---
+++ model.py
@@ -0,0 +1,24 @@
+# -*- encoding=utf8 -*-
+"""
+@author: Jerry
+@contact: lvjy3.15@sem.tsinghua.edu.com
+@file: model.py
+@time: 2016/11/20 19:13
+"""
+
+from utils import *
+
+
+def predict(train, test, type=0):
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    train_single_set, train_multi_set, test_single_set, test_multi_set = prepare_data()
+    test_single_pred = predict(train_single_set, test_single_set)
+    test_multi_pred = predict(train_multi_set, test_multi_set)
